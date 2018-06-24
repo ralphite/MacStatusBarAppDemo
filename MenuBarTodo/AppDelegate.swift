b729467f523f64ac89e37d7695f61163c0e41821
Yadong Wen <yadongwen@gmail.com>
2018-06-24 03:08:49 -0700

    Initial Commit

---
+++ MenuBarTodo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MenuBarTodo
+//
+//  Created by Yadong Wen on 6/24/18.
+//  Copyright © 2018 Yadong Wen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MenuBarTodo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuBarTodo
+//
+//  Created by Yadong Wen on 6/24/18.
+//  Copyright © 2018 Yadong Wen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
